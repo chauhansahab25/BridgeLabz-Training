330e4e5cbb9c23a50293054cf815eadc8892ba36
chauhansahab25 <priyanshuchauhan2509@gmail.com>
2025-12-28 13:21:58 +0530

    Priyanshu chauhan Added level 1 problems to csharp-strings

---
+++ core-csharp-practice/gcr-codebase/csharp-strings/Level 1/CompareTwoString.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CG_Practice.Strings
+{
+    public class CompareTwoString
+    {
+        static bool CompareUsingCharAt(string str1, string str2)
+        {
+            if (str1.Length != str2.Length)
+                return false;
+
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (str1[i] != str2[i])
+                    return false;
+            }
+            return true;
+        }
+        static void Main()
+        {
+            Console.WriteLine("Enter first String:");
+            string inp1 = Console.ReadLine();
+
+            Console.WriteLine("Enter second String:");
+            string inp2 = Console.ReadLine();
+
+            bool cstmrslt = CompareUsingCharAt(inp1,inp2);
+            Console.WriteLine($" Custom CharAt comparison: {cstmrslt}");
+
+            bool builtinrslt = inp1.Equals(inp2);
+            Console.WriteLine($"Built-in Equals comparison: {builtinrslt}");
+
+
+            Console.WriteLine($"Result match: {cstmrslt == builtinrslt}");
+        }
+
+    }
+}
