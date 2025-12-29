dd27a76f37ebe073922c36d9ea62c35021098580
chauhansahab25 <priyanshuchauhan2509@gmail.com>
2025-12-29 10:46:23 +0530

    Priyanshu chauhan Added Level 1 problems to csharp-strings

---
+++ core-csharp-practice/gcr-codebase/csharp-strings/Level 1/VowelConsonantCounter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CG_Practice.Strings
+{
+    class VowelConsonantCounter
+    {
+        static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+            CountVowelsAndConsonants(input);
+        }
+        static void CountVowelsAndConsonants(string text)
+        {
+            int vow = 0, cons = 0;
+            text = text.ToLower();   //string function
+
+            foreach (char ch in text)
+            {
+                if (char.IsLetter(ch))
+                {
+                    if ("aeiou".Contains(ch))
+                        vow++;
+                    else
+                        cons++;
+                }
+            }
+
+            Console.WriteLine("Vowels: " + vow);
+            Console.WriteLine("Consonants: " + cons);
+        }
+    }
+
+}
