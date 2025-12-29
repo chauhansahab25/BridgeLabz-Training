10336a3c2a9aa118234a8becedced19380acd046
chauhansahab25 <priyanshuchauhan2509@gmail.com>
2025-12-29 10:39:42 +0530

    Priyanshu chauhan Added Level 2 problems to csharp-build-in-function

---
+++ core-csharp-practice/gcr-codebase/csharp-build-in-function/Level 2/MaxOfThree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CG_Practice.Build_In_Function
+{
+    public class MaxOfThree
+    {
+        static int FindMax(int a, int b, int c)
+        {
+            return Math.Max(a, Math.Max(b, c));
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter three numbers: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Maximum: " + FindMax(a, b, c));
+        }
+    }
+}
---
+++ core-csharp-practice/gcr-codebase/csharp-build-in-function/Level 2/Palindrome (1).cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CG_Practice.Build_In_Function
+{
+    public class Palindrome
+    {
+        static bool IsPalindrome(string text)
+        {
+            int start = 0, end = text.Length - 1;
+
+            while (start < end)
+            {
+                if (text[start] != text[end]) return false;
+                start++;
+                end--;
+            }
+
+            return true;
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter a string: ");
+            string text = Console.ReadLine();
+
+            Console.WriteLine(IsPalindrome(text) ? "Palindrome" : "Not a Palindrome");
+        }
+    }
+}
