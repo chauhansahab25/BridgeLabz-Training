4296b9b9eef72037c6dabd88a625bcd4c96849b9
chauhansahab25 <priyanshuchauhan2509@gmail.com>
2026-01-06 11:25:03 +0530

    Priyanshu Chauhan Added Vehical Rental Application  to  scenario based problem

---
+++ oops-csharp-practice/scenario-based/VehicleRentalApplication/Bike.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CG_Practice.oopsscenario.VehicleRentalApplication
+{
+        class Bike : Vehicle, IInsurable
+        {
+            private int engineCC;
+
+            public int EngineCC { get { return engineCC; } set { engineCC = value; } }
+
+            public Bike(string number, double rate, int cc) : base(number, "Bike", rate)
+            {
+                engineCC = cc;
+            }
+
+            public override double CalculateRentalCost(int days)
+            {
+                return RentalRate * days * 0.8; // 20% discount for bikes
+            }
+
+            public double CalculateInsurance()
+            {
+                return RentalRate * 0.10; // 10% of rental rate
+            }
+
+            public string GetInsuranceDetails()
+            {
+                return "Bike Insurance - Policy: " + PolicyNumber;
+            }
+        }
+    
+}
