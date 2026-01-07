4bafd130bc15ab3f058bedcea1de4b7be41372d3
chauhansahab25 <priyanshuchauhan2509@gmail.com>
2026-01-07 15:53:57 +0530

    Priyanshu chauhan added uc6 files in Employee Wage Computation

--- oops-csharp-practice/scenario-based/EmployeeWageComputation/EmployeeWage.cs
+++ oops-csharp-practice/scenario-based/EmployeeWageComputation/EmployeeWage.cs
@@ -9,6 +9,8 @@ namespace CG_Practice.oopsscenario.EmployeeWageComputation
         public const int FullDayHour = 8;
         public const int PartTimeHour = 4; // uc3 function
         public const int WorkingDaysPerMonth = 20; // uc5 function
+        public const int MaxWorkingHours = 100; // uc6 function
+        public const int MaxWorkingDays = 20; // uc6 function
         // uc4 function - constants for switch case
         public const int IsAbsent = 0;
         public const int IsPartTime = 1;
@@ -21,8 +23,7 @@ namespace CG_Practice.oopsscenario.EmployeeWageComputation
 
             EmployeeWage emp = new EmployeeWage();
             
-            int monthlyWage = emp.CalculateMonthlyWage(); // uc5 function
-            Console.WriteLine("Monthly Wage: $" + monthlyWage);
+            emp.CalculateWageTillCondition(); // uc6 function
 
             Console.ReadKey();
         }
@@ -76,6 +77,46 @@ namespace CG_Practice.oopsscenario.EmployeeWageComputation
                 totalWage += CalculateDailyWage();
             }
             return totalWage;
+        }
+
+        // uc6 function
+        public void CalculateWageTillCondition()
+        {
+            Random rand = new Random();
+            int totalWage = 0;
+            int totalHours = 0;
+            int totalDays = 0;
+
+            while (totalHours < MaxWorkingHours && totalDays < MaxWorkingDays)
+            {
+                totalDays++;
+                int attendance = rand.Next(0, 3);
+                int dailyWage = 0;
+                int workHours = 0;
+
+                switch (attendance)
+                {
+                    case IsPartTime:
+                        workHours = PartTimeHour;
+                        dailyWage = WagePerHour * PartTimeHour;
+                        break;
+                    case IsFullTime:
+                        workHours = FullDayHour;
+                        dailyWage = WagePerHour * FullDayHour;
+                        break;
+                    case IsAbsent:
+                        workHours = 0;
+                        dailyWage = 0;
+                        break;
+                }
+
+                totalHours += workHours;
+                totalWage += dailyWage;
+            }
+
+            Console.WriteLine("Total Days: " + totalDays);
+            Console.WriteLine("Total Hours: " + totalHours);
+            Console.WriteLine("Total Wage: $" + totalWage);
         }
     }
 }
