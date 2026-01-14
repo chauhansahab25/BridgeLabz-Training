1cd25a685e29f9ce1477654225d033a5c7ebdc29
chauhansahab25 <priyanshuchauhan2509@gmail.com>
2026-01-14 16:42:47 +0530

    Priyanshu chauhan Added BookBuddy digital bookshelf application

---
+++ dsa-csharp-practice/scenario-based/BookBuddy/BookBuddyApp.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CG_Practice.dsascenario.BookBuddy
+{
+    class BookBuddyApp
+    {
+        static void Main(string[] args)
+        {
+            new Menu().Start();
+        }
+    }
+}
