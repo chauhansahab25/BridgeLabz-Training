c0da4badc02e6cab70b113f61e14354d9ee8d2a6
chauhansahab25 <priyanshuchauhan2509@gmail.com>
2026-01-19 15:08:51 +0530

    Priyanshu Chauhan Added AI Resume Screening to gcr-codebase

---
+++ collection-csharp-practice/gcr-codebase/collection-generics/AIResumeScreening/Resume.cs
@@ -0,0 +1,23 @@
+// using System;
+// using System.Collections.Generic;
+
+// namespace CG_Practice.dsascenario.AIResumeScreening
+// {
+//     public class Resume<T> where T : JobRole
+//     {
+//         private List<T> list = new List<T>();
+
+//         public void Add(T role)
+//         {
+//             list.Add(role);
+//         }
+
+//         public void Process()
+//         {
+//             foreach (var r in list)
+//             {
+//                 r.Check();
+//             }
+//         }
+//     }
+// }
---
+++ collection-csharp-practice/gcr-codebase/collection-generics/AIResumeScreening/ResumeUtility.cs
@@ -0,0 +1,13 @@
+// using System;
+
+// namespace CG_Practice.dsascenario.AIResumeScreening
+// {
+//     public class ResumeUtility
+//     {
+//         public static void AddRole<T>(Resume<T> res, T role)
+//             where T : JobRole
+//         {
+//             res.Add(role);
+//         }
+//     }
+// }
