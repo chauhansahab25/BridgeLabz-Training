f8c598f8fd4ad0231bb1fe11373cd0dcacf49937
chauhansahab25 <priyanshuchauhan2509@gmail.com>
2026-01-19 15:05:21 +0530

    Priyanshu Chauhan Added Smart Warehouse Management System to gcr-codebase

---
+++ collection-csharp-practice/gcr-codebase/collection-generics/SmartWarehouseManagementSystem/Furniture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace CG_Practice.dsascenario.SmartWarehouseManagementSystem
+{
+ 
+        public class Furniture : WareHouseSystem
+        {
+            public string Material { get; set; }
+
+            public Furniture(string name, double price, string material)
+                : base(name, price)
+            {
+                Material = material;
+            }
+
+            public override void Show()
+            {
+                Console.WriteLine($"Furniture: {Name}, Price: {Price}, Material: {Material}");
+            }
+        }
+    }
+
---
+++ collection-csharp-practice/gcr-codebase/collection-generics/SmartWarehouseManagementSystem/Groceries.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace CG_Practice.dsascenario.SmartWarehouseManagementSystem
+{
+        public class Groceries : WareHouseSystem
+        {
+            public DateTime ExpiryDate { get; set; }
+
+            public Groceries(string name, double price, DateTime expiry)
+                : base(name, price)
+            {
+                ExpiryDate = expiry;
+            }
+
+            public override void Show()
+            {
+                Console.WriteLine($"Grocery: {Name}, Price: {Price}, Expiry: {ExpiryDate.ToShortDateString()}");
+            }
+        }
+    }
+
