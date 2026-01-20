eecfb858e9e5d6bbe740f1dd4bf7b59408649f2b
chauhansahab25 <priyanshuchauhan2509@gmail.com>
2026-01-20 15:40:59 +0530

    Priyanshu Chauhan Added Exam Proctor to scenario-based

---
+++ dsa-csharp-practice/scenario-based/ExamProctor/ExamProctorApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG_Practice.dsascenario.ExamProctor
+{
+    class ExamProctorApp
+    {
+        static void Main(string[] args) {
+            new ExamMenu().Start();
+        }
+    }
+}
---
+++ dsa-csharp-practice/scenario-based/ExamProctor/QuestionStack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG_Practice.dsascenario.ExamProctor
+{
+    class QuestionStack
+    {
+        private int[] stack;
+        private int top;
+        public QuestionStack(int size)
+        {
+            stack = new int[size];
+            top = -1;
+        }
+        public void Push(int value)
+        {
+            if (top < stack.Length - 1)
+            {
+                stack[++top] = value;
+            }
+        }
+        public int Peek()
+        {
+            if (top >= 0)
+                return stack[top];
+            return -1;
+        }
+    }
+}
