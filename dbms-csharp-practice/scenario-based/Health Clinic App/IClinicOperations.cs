5295ddc862bf5eac151b650870bca71b1b887adb
chauhansahab25 <priyanshuchauhan2509@gmail.com>
2026-02-07 15:49:17 +0530

    Priyanshu Chauhan Added UC 1.1 of Health Clinic App to scenario-based

---
+++ dbms-csharp-practice/scenario-based/Health Clinic App/IClinicOperations.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace HealthClinicApp
+{
+    public interface IClinicOperations
+    {
+        void RegisterNewPatient();
+        void UpdatePatientInformation();
+    }
+}
---
+++ dbms-csharp-practice/scenario-based/Health Clinic App/Utility.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HealthClinicApp
+{
+    public class Utility
+    {
+        public static void PressAnyKey()
+        {
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
