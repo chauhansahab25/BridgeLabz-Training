c9876070a491e5e8ee6fb0452cea696b5e318865
Priyanshu_chauhan25 <priyanshuchauhan2509@gmail.com>
2026-08-02 21:40:57 +0530

    Complete Day 1 Introduction to Data Structures and Primitive Data Structures

---
+++ Day-1/Mini-Max-Sum.cs
@@ -0,0 +1,48 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static void miniMaxSum(List<int> arr)
+    {
+        long total=0;
+        int min=arr[0];
+        int max=arr[0];
+        for(int i=0; i<arr.Count; i++)
+        {
+            total+=arr[i];
+            if(arr[i]<min)
+            {
+                min=arr[i];
+            }
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+    }
+    Console.WriteLine((total-max)+ " " + (total-min));
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
+}
